8fdf634219c7227257734974c0387cbc24824052
Ivan Lopes <ivan (at) 42algoritmos (dot) com (dot) br>
2017-05-06 06:49:18 -0300

    Atualiza o arquivo etc/main.mk

--- etc/main.mk
+++ etc/main.mk
@@ -1,3 +1,9 @@
+MARKDOWN=\
+STEPS.md         \
+doc/license.md   \
+doc/credits.md   \
+doc/self-promotion.md
+
 update:
 	android update project --name $(NAME) --target $(TARGET) -p .
 
@@ -5,7 +11,7 @@ jenv:
 	jenv local oracle64-1.8.0.131
 
 README.md:
-	@cat ETAPAS.md| ./gh-md-toc - | sed 's/^ \+//' > $@
+	@cat $(MARKDOWN)| ./gh-md-toc - | sed 's/^ \+//' > $@
 	@echo ''                 >> $@
 	@echo ''                 >> $@
 	@sed '1,2d' ETAPAS.md    >> $@
